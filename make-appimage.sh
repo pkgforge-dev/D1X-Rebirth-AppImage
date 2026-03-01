f7cabdeea391a05ce55a85d8b0fa3aa4b5e6429f
Link4Electronics <link4electronics@gmail.com>
2026-03-01 05:01:39 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,18 +3,16 @@
 set -eu
 
 ARCH=$(uname -m)
-#VERSION=$(pacman -Q dxx-rebirth-git | awk '{print $2; exit}') # example command to get version of application here
-export ARCH #VERSION
+export ARCH
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-#export ICON=https://raw.githubusercontent.com/dxx-rebirth/dxx-rebirth/refs/heads/master/contrib/packaging/linux/descent.svg
-#export DESKTOP=/usr/share/applications/d1x-rebirth.desktop
 export STARTUPWMCLASS=
 export DEPLOY_OPENGL=1
 
 # Deploy dependencies
 quick-sharun ./AppDir/bin/d1x-rebirth
+echo 'SHARUN_WORKING_DIR=${SHARUN_DIR}/bin' >> ./AppDir/.env
 
 # Additional changes can be done in between here
 
