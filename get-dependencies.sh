83472cdf696e7e24645128210f6b926888225b8f
Link4Electronics <link4electronics@gmail.com>
2026-03-01 05:02:19 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -21,7 +21,7 @@ echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
-#make-aur-package dxx-rebirth-git
+#make-aur-package
 
 # If the application needs to be manually built that has to be done down here
 echo "Making nightly build of D1X-Rebirth..."
