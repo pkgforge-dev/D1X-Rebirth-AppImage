1863096517e43ed7a0904bdddb8613580ecbc7ec
Link4Electronics <link4electronics@gmail.com>
2026-08-23 18:05:33 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -47,5 +47,3 @@ scons "${_common_opts[@]}" 'd1x=1' 'd2x=0'
 
 mv -v build/d1x-rebirth/d1x-rebirth "d1x-rebirth addons/d1xr-hires.dxa" "d1x-rebirth addons/d1xr-sc55-music.dxa" ../AppDir/bin
 mv -v d1x-rebirth/d1x-rebirth.desktop ../AppDir
-cp contrib/packaging/linux/descent.svg ../AppDir/.DirIcon
-mv -v contrib/packaging/linux/descent.svg ../AppDir
