133084f7e9638c1915d447daf24b12d8ffed3f2b
Link4Electronics <link4electronics@gmail.com>
2026-08-25 10:24:32 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -18,7 +18,3 @@ quick-sharun ./AppDir/bin/d1x-rebirth /usr/lib/libfluidsynth.so*
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
-
-# Test the app for 12 seconds, if the test fails due to the app
-# having issues running in the CI use --simple-test instead
-quick-sharun --simple-test ./dist/*.AppImage
